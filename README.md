3c46d15edac883ce0f2b39f74809493013acadfe
James Brown <Roguelazer@gmail.com>
2013-08-09 11:14:53 -0700

    Work with python3000, support automatic iterations determination

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 This is a little demo of why it's bad to use non-constant-time string comparison functions in security-sensitive code.
 
 The demo is pretty sensitive to system load if you want to be able to run it in a reasonable amount of time. On a very quiet system,
-you can usually get good results by running with `-i 100000`. On an interactive system with lots of interrupts, you need to increase
+you can usually get good results by running with `-i 100`. On an interactive system with lots of interrupts, you need to increase
 that by a few orders of magnitude to get reliable results.
+
+It uses numpy and scipy, and should run on python 2.6, 2.7, or 3.3
--- timing_attack.py
+++ timing_attack.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import collections
 import gc
 import hashlib
@@ -10,12 +12,21 @@ import sys
 
 import scipy.stats
 
+MIN_AUTO_ITERATIONS = 20
+MAX_AUTO_ITERATIONS = 10240
+
 try:
     import time33
     timer = time33.perf_counter
 except:
     import time
-    timer = time.clock
+    try:
+        timer = time.perf_counter
+    except:
+        if sys.platform == 'darwin' or sys.platform == 'win32':
+            timer = time.clock  # time.clock is oddly-low-resolution on linux
+        else:
+            timer = time.time
 
 
 def compare_character(got, expected):
@@ -29,21 +40,24 @@ def compare_token(got, expected):
     """
     if len(got) != len(expected):
         return False
-    for i in xrange(len(got)):
+    for i in range(len(got)):
         if not compare_character(got[i], expected[i]):
             return False
     # just to be extra sure, also compare the hashes (yes, we're cheating
     # here to make it easier to guess the last character)
-    return hashlib.sha1(''.join(got)).hexdigest() == hashlib.sha1(''.join(expected)).hexdigest()
+    got_enc = (''.join(got)).encode('utf-8')
+    expected_enc = (''.join(expected)).encode('utf-8')
+    return hashlib.sha1(got_enc).digest() == hashlib.sha1(expected_enc).digest()
 
 
 def guess_and_time(init_guess, expected, iterations, token_length):
     tofill = range(token_length - len(init_guess))
     # do a collection before we start to try and reduce variance inside the
     # loop
-    gc.collect()
     results = numpy.zeros(iterations)
-    for i in xrange(iterations):
+    gc.collect()
+    gc.disable()
+    for i in range(iterations):
         # fill with rotating data to avoid biasing the results
         guess = init_guess + [i % 10 for _ in tofill]
         start = timer()
@@ -51,6 +65,7 @@ def guess_and_time(init_guess, expected, iterations, token_length):
         end = timer()
         this_time = (1000000.0 * (end - start))
         results[i] = this_time
+    gc.enable()
     gc.collect()
 
     return results
@@ -58,23 +73,27 @@ def guess_and_time(init_guess, expected, iterations, token_length):
 
 def main():
     parser = optparse.OptionParser()
-    parser.add_option('-i', '--iterations', default=100, type=int, help='Number of iterations to run for (default %default)')
+    parser.add_option('-i', '--iterations', default=None, type=int, help='Number of iterations to run for (if not passed, will be AUTOMAGIC)')
     parser.add_option('-c', '--confidence-threshold', default=0.02, type=float, help='Require p < CONFIDENCE_THRESHOLD for a digit (default %default)')
     parser.add_option('-t', '--token-length', default=5, type=int, help='How long to make the token (default %default, ignored if you pass --token)')
     parser.add_option('-v', '--verbose', action='store_true', help='Be verbose')
     parser.add_option('-n', '--numeric', default=False, action='store_true', help='Only generate numeric tokens')
+    parser.add_option('-T', '--token', default=None, help='Token (for advanced use)')
     opts, args = parser.parse_args()
 
     if opts.numeric:
-        language = map(str, range(10))
+        language = list(map(str, range(10)))
     else:
-        language = map(chr, range(ord('a'), ord('z')+1) + range(ord('A'), ord('Z') + 1) + range(ord('0'), ord('9') + 1))
-    expected = [random.choice(language) for i in range(opts.token_length)]
+        language = list(map(chr, list(range(ord('a'), ord('z')+1)) + list(range(ord('A'), ord('Z') + 1)) + list(range(ord('0'), ord('9') + 1))))
+    if opts.token:
+        expected = opts.token
+    else:
+        expected = [random.choice(language) for i in range(opts.token_length)]
 
     token_length = len(expected)
 
-    print "This program isn't perfect, but it will often guess the random token using nothing more than a bad comparison function."
-    print "Token is %s" % ''.join(map(str, expected))
+    print("This program isn't perfect, but it will often guess the random token using nothing more than a bad comparison function.")
+    print("Token is %s" % ''.join(map(str, expected)))
 
     overall_guess = []
 
@@ -82,45 +101,62 @@ def main():
     Result = collections.namedtuple('Result', ['digit', 'mean', 'p_value', 'stddev'])
     FinalResult = collections.namedtuple('FinalResult', ['digit', 'mean', 'p_values', 'stddev'])
 
-    for character in xrange(token_length):
-        sys.stdout.write("> ")
+    last_iterations = None
+    for character in range(token_length):
         sys.stdout.flush()
-        all_times = []
-        for digit in language:
-            guess = overall_guess + [digit]
-            times = guess_and_time(guess, expected, opts.iterations, token_length)
-            time = numpy.mean(times)
-            all_times.append((digit, time, times))
-            sys.stdout.write(". ")
-            sys.stdout.flush()
-        sys.stdout.write("\n")
-
-        all_data = numpy.zeros(len(all_times) * opts.iterations)
-        for i, (_, __, times) in enumerate(all_times):
-            for j, v in enumerate(times):
-                all_data[i*opts.iterations + j] = v
-
-        average_time = numpy.mean([t[1] for t in all_times])
+        if opts.iterations is not None:
+            min_iterations = last_iterations or opts.iterations
+            max_iterations = opts.iterations
+        else:
+            min_iterations = last_iterations or MIN_AUTO_ITERATIONS
+            max_iterations = MAX_AUTO_ITERATIONS
+        iterations = min_iterations
         candidates = []
-        for digit, mean, times in all_times:
-            std = numpy.std(times)
-            candidates.append(Candidate(digit, mean, times, std))
         p_candidates = []
-        failed = []
-        # Find all of the digits that are stastically different
-        for result1, result2 in itertools.product(candidates, candidates):
-            if result1.digit == result2.digit:
-                continue
-            t_value, p_value = scipy.stats.ttest_ind(result1.times, result2.times, equal_var=False)
-            if p_value < opts.confidence_threshold:
-                p_candidates.append(Result(result1.digit, result1.mean, p_value, result1.stddev))
-                p_candidates.append(Result(result2.digit, result2.mean, p_value, result2.stddev))
-            else:
-                failed.append(Result(result1.digit, result1.mean, p_value, result1.stddev))
+        while iterations <= max_iterations:
+            sys.stdout.write("> (iterations=%d) " % iterations)
+            all_times = []
+            for digit in language:
+                guess = overall_guess + [digit]
+                times = guess_and_time(guess, expected, iterations, token_length)
+                time = numpy.mean(times)
+                all_times.append((digit, time, times))
+                sys.stdout.write(". ")
+                sys.stdout.flush()
+            sys.stdout.write("\n")
+
+            all_data = numpy.zeros(len(all_times) * iterations)
+            for i, (_, __, times) in enumerate(all_times):
+                for j, v in enumerate(times):
+                    all_data[i*iterations + j] = v
+
+            average_time = numpy.mean([t[1] for t in all_times])
+            for digit, mean, times in all_times:
+                std = numpy.std(times)
+                candidates.append(Candidate(digit, mean, times, std))
+            failed = []
+            # Find all of the digits that are stastically different using
+            # t-tests
+            # XXX WARNING XXX: Doing paired t-tests like this isn't exactly the
+            # best way to accomplish this
+            for result1, result2 in itertools.product(candidates, candidates):
+                if result1.digit == result2.digit:
+                    continue
+                t_value, p_value = scipy.stats.ttest_ind(result1.times, result2.times, equal_var=False)
+                if p_value < opts.confidence_threshold:
+                    p_candidates.append(Result(result1.digit, result1.mean, p_value, result1.stddev))
+                    p_candidates.append(Result(result2.digit, result2.mean, p_value, result2.stddev))
+                else:
+                    failed.append(Result(result1.digit, result1.mean, p_value, result1.stddev))
+            if p_candidates:
+                last_iterations = iterations
+                break
+            if not p_candidates:
+                print("No candidates were under confidence threshold %f (best %f)" % (opts.confidence_threshold, min(p.p_value for p in failed)))
+                print("Retrying with higher iteration count")
+                iterations *= 2
         if not p_candidates:
-            print >>sys.stderr, "No candidates were under confidence threshold %f" % opts.confidence_threshold
-            print >>sys.stderr, "Retry with higher iteration count"
-            print failed
+            print("No candidates found under confidence threshold %f, exhauted iterations" % opts.confidence_threshold)
             break
         # Now find the most common
         counts = dict((d, len([p for p in p_candidates if p.digit == d])) for d in language)
@@ -133,11 +169,11 @@ def main():
             results.append(FinalResult(digit=candidate_entry.digit, mean=candidate_entry.mean, p_values=p_values, stddev=candidate_entry.stddev))
         results.sort(key=operator.attrgetter('mean'), reverse=True)
         if opts.verbose:
-            print results
+            print(results)
         digit, mean, p_values, std = results[0]
-        print "Will guess %s (in %.1f +/- %0.1f micros, average %.1f micros, max_p_value=%f)" % (digit, mean, std, average_time, max(p_values))
+        print("Will guess %s (in %.1f +/- %0.1f micros, average %.1f micros, max_p_value=%f)" % (digit, mean, std, average_time, max(p_values)))
         overall_guess.append(digit)
-    print "I think the token is %s%s" % (''.join(map(str, overall_guess)), ''.join('?' for _ in xrange(token_length - len(overall_guess))))
+    print("I think the token is %s%s" % (''.join(map(str, overall_guess)), ''.join('?' for _ in range(token_length - len(overall_guess)))))
 
 
 if __name__ == '__main__':
